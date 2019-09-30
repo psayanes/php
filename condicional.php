a41b3021d3501429717d3c9259c9ffbd0aed2635
psayanes <psayanes@gmail.com>
2019-09-30 19:40:37 -0300

    xxx

---
+++ condicional.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Condicion IF</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1 style="color: black">Condicion IF</h1>
+    <?php
+    $edad = 18;
+    if ($edad >= 18){
+        echo "Como tiene $edad puede entrar porque es mayor de 18 años";
+    } else {
+        echo "Como tiene $edad no puede entrar porque no es mayor de 18 años";
+
+    }
+    ?>
+</body>
+</html>
